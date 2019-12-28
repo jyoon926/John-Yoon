1cf05b33813ecdae92ff0798b3899e35b41332da
Jacob Yoon <jacobyoon926@gmail.com>
2019-12-28 12:36:08 -0500

    Remove boxShadow

--- js/script.js
+++ js/script.js
@@ -5,11 +5,9 @@ window.onscroll = function() {
 function scrollFunction() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         document.getElementById("header").style.height = "60px";
-        document.getElementById("header").style.boxShadow = "0 2px 5px rgba(0,0,0,0.3)";
         document.getElementById("logo").style.height = "28px";
     } else {
         document.getElementById("header").style.height = "120px";
         document.getElementById("logo").style.height = "40px";
-        document.getElementById("header").style.boxShadow = "0 2px 5px rgba(0,0,0,0)";
     }
 }
