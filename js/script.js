1019f63e60b1619e8070402af9c5e81c4a1595d8
Jacob Yoon <jacobyoon926@gmail.com>
2019-12-30 11:18:30 -0500

    Fix samples-kroc

--- js/script.js
+++ js/script.js
@@ -23,24 +23,28 @@ function scrollFunction() {
 }
 
 //Audio Player
+//Audio Player
 jQuery(document).ready(function(){
     var aud = $('audio')[0];
-    var play = $('.play');
-    var progess = $('.progress');
+    var play = $('.play1');
+    var progess = $('.progress1');
+    var playbtn = $('.playbtn1');
+    var cur = $('.current1');
+    var cont = $('.cont1');
     var percent = aud.currentTime / aud.duration;
     var current = aud.currentTime;
     play.click(function() {
         if (aud.paused) {
             aud.play();
-            $('.playbtn').attr("src", "img/pause.svg");
+            playbtn.attr("src", "img/pause.svg");
         } else {
             aud.pause();
-            $('.playbtn').attr("src", "img/play.svg");
+            playbtn.attr("src", "img/play.svg");
         }
     })
-    $('.cont').click(function(e) {
+    cont.click(function(e) {
         var posX = $(this).position().left;
-        percent = (e.pageX - posX) / $('.cont').width();
+        percent = (e.pageX - posX) / cont.width();
         progess.css('width', percent * 100 + '%');
         current = aud.duration * percent;
         aud.currentTime = percent * aud.duration;
@@ -50,6 +54,78 @@ jQuery(document).ready(function(){
         var min = parseInt(aud.currentTime / 60);
         var s = parseInt(aud.currentTime) - (60 * min);
         var sec = ("0" + s).slice(-2);
-        $('.current').text(min + ":" + sec);
+        cur.text(min + ":" + sec);
+    }
+})
+
+
+jQuery(document).ready(function(){
+    var aud = $('audio')[1];
+    var play = $('.play2');
+    var progess = $('.progress2');
+    var playbtn = $('.playbtn2');
+    var cur = $('.current2');
+    var cont = $('.cont2');
+    var percent = aud.currentTime / aud.duration;
+    var current = aud.currentTime;
+    play.click(function() {
+        if (aud.paused) {
+            aud.play();
+            playbtn.attr("src", "img/pause.svg");
+        } else {
+            aud.pause();
+            playbtn.attr("src", "img/play.svg");
+        }
+    })
+    cont.click(function(e) {
+        var posX = $(this).position().left;
+        percent = (e.pageX - posX) / cont.width();
+        progess.css('width', percent * 100 + '%');
+        current = aud.duration * percent;
+        aud.currentTime = percent * aud.duration;
+    })
+    aud.ontimeupdate = function(){
+        progess.css('width', aud.currentTime / aud.duration * 100 + '%');
+        var min = parseInt(aud.currentTime / 60);
+        var s = parseInt(aud.currentTime) - (60 * min);
+        var sec = ("0" + s).slice(-2);
+        cur.text(min + ":" + sec);
+    }
+})
+
+
+
+
+jQuery(document).ready(function(){
+    var aud = $('audio')[2];
+    var play = $('.play3');
+    var progess = $('.progress3');
+    var playbtn = $('.playbtn3');
+    var cur = $('.current3');
+    var cont = $('.cont3');
+    var percent = aud.currentTime / aud.duration;
+    var current = aud.currentTime;
+    play.click(function() {
+        if (aud.paused) {
+            aud.play();
+            playbtn.attr("src", "img/pause.svg");
+        } else {
+            aud.pause();
+            playbtn.attr("src", "img/play.svg");
+        }
+    })
+    cont.click(function(e) {
+        var posX = $(this).position().left;
+        percent = (e.pageX - posX) / cont.width();
+        progess.css('width', percent * 100 + '%');
+        current = aud.duration * percent;
+        aud.currentTime = percent * aud.duration;
+    })
+    aud.ontimeupdate = function(){
+        progess.css('width', aud.currentTime / aud.duration * 100 + '%');
+        var min = parseInt(aud.currentTime / 60);
+        var s = parseInt(aud.currentTime) - (60 * min);
+        var sec = ("0" + s).slice(-2);
+        cur.text(min + ":" + sec);
     }
 })
